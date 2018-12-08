88e72c4e15c5707c416830c29bc6cfdfbcbb332e
Nathan Lee <nathanlee@pop-os.localdomain>
2018-12-07 23:03:59 -0800

    Added backend logic

--- Webappnondocker/litsearch/classifier.py
+++ Webappnondocker/litsearch/classifier.py
@@ -1,7 +1,16 @@
 from nltk import tokenize
+import random
 
-text = ''
-def sentencesplit(text):
-    splittext = ''
-    return splittext
 
+def classify(text, theme):
+    splittext = tokenize.sent_tokenize(text)
+
+    highlighted = []
+
+    #replaced with classifier
+
+    for sentences in splittext:
+        test = random.randint(1)
+        if test == 0:
+            highlighted.append(sentences)
+    return highlighted
--- Webappnondocker/litsearch/views.py
+++ Webappnondocker/litsearch/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from litsearch.classifier import classify
 
 
 def index(request):
     if request.method == 'GET':
         return render(request, 'index.html', {})
+
+    if request.method == 'POST':
+        returned = classify(request.POST['text'], request.POST['theme'])
+        return render(request, {'highlighted': returned})
